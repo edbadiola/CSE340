59b0a414b03342d13ac76e0987df06fdd0087318
edbadiola <u000681426@byui.edu>
2025-09-18 17:29:32 +0800

    added style to the inventory route pages

--- utilities/index.js
+++ utilities/index.js
@@ -30,32 +30,33 @@ Util.getNav = async function (req, res, next) {
 Util.buildClassificationGrid = async function(data){
   let grid
   if(data.length > 0){
-    grid = '<ul id="inv-display">'
+    // add our outer class
+    grid = '<div class="inv-grid">'
     data.forEach(vehicle => { 
-      grid += '<li>'
+      grid += '<div class="inv-card">'  // wrapper for each vehicle
       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>'
+        + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model + ' details">'
+        + '<img src="' + vehicle.inv_thumbnail 
+        + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model + ' on CSE Motors" />'
+        + '</a>'
       grid += '<div class="namePrice">'
       grid += '<hr />'
       grid += '<h2>'
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+      grid +=   '<a href="../../inv/detail/' + vehicle.inv_id + '" title="View ' 
+        + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+        + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
       grid += '</h2>'
-      grid += '<span>$' 
-      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      grid += '<span>$' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
       grid += '</div>'
-      grid += '</li>'
+      grid += '</div>'  // close .inv-card
     })
-    grid += '</ul>'
+    grid += '</div>'    // close .inv-grid
   } else { 
-    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+    grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>'
   }
   return grid
 }
+
 
 /* ****************************************
  * Middleware For Handling Errors
